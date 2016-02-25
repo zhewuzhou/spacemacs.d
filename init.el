93723f548533e111c85a6eea191d2a6089b2b71b
zhewuzhou <zhewuzhou@gmail.com>
2016-02-25 16:39:55 +0800

    [Z.Z][Spacemacs] global keybinds

--- init.el
+++ init.el
@@ -278,6 +278,7 @@ layers configuration. You are free to put any user code."
   (add-to-list 'load-path (expand-file-name "lisp" "~/.spacemacs.d/"))
   (require 'init-linum)
   (require 'init-org)
+  (require 'init-global-keybinds)
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
---
+++ lisp/init-global-keybinds.el
@@ -0,0 +1,42 @@
+;;; init-global-keybinds.el start here
+(evil-leader/set-key
+  ;;; don't use o, because it's the namespace of org
+  "aa" 'avy-goto-char-2
+  "aA" 'helm-apropos
+  "W"  'ispell-word
+  "dw" 'delete-other-windows
+  "db" 'delete-all-other-buffers
+  "fm" 'indent-region
+  "pF" 'helm-projectile-find-file-in-known-projects
+  "m"  'markdown-preview
+  "nt" 'neotree-toggle
+  ;;; o is for org mode
+  "Q"  'save-buffers-kill-terminal
+  "ds" 'delete-trailing-whitespace
+  "Z"  'current-term)
+
+(defun find-term-buffer ()
+  (interactive)
+  (car (-filter (lambda (b)
+                  (eq 'term-mode (buffer-local-value 'major-mode b)))
+                (buffer-list))))
+
+;;; this is the setting for kill buffer without confirmation
+;;; from http://emacs.stackexchange.com/questions/14509/kill-process-buffer-without-confirmation
+(setq kill-buffer-query-functions (delq 'process-kill-buffer-query-function kill-buffer-query-functions))
+
+(defun kill-term ()
+  (interactive)
+  (if (find-term-buffer)
+      (kill-buffer (find-term-buffer)))
+  )
+
+(defun current-term ()
+  (interactive)
+  (if (find-term-buffer)
+      (switch-to-buffer (buffer-name (find-term-buffer)))
+    (ansi-term (getenv "SHELL") "editing-file-root")
+    ))
+
+(provide 'init-global-keybinds)
+;;; init-global-keybinds.el end here
