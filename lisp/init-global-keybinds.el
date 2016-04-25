9e8c74410d88fbb4635a661408ca3a3ea28758df
zhewuzhou <zhewuzhou@gmail.com>
2016-04-25 11:31:17 +0800

    [Z.Z][GolbalKey] Adding pyim convert pinying and do buffer format as well

--- lisp/init-global-keybinds.el
+++ lisp/init-global-keybinds.el
@@ -1,29 +1,23 @@
 ;;; init-global-keybinds.el start here
 (evil-leader/set-key
-  ;;; don't use o, because it's the namespace of org
+;;; don't use o, because it's the namespace of org
   ;;; making sure always using 2 chars single chars make it less flexable
-  ",A" 'diff-region-tag-selected-as-a
-  ",B" 'diff-region-compare-with-b
-  ",b" 'helm-mini
-  ",c" 'avy-goto-word-1
-  ",w" 'avy-goto-char-2
-  ",g" 'google-translate-at-point
+  ",A" 'diff-region-tag-selected-as-a ",B" 'diff-region-compare-with-b
+  ",b" 'helm-mini ",c" 'avy-goto-word-1 ",w"
+  'avy-goto-char-2 ",g" 'google-translate-at-point
   ",t" 'google-translate-query-translate-reverse
-  "aA" 'helm-apropos
-  "cw" 'ispell-word
-  "wD" 'delete-other-windows
-  "db" 'delete-all-other-buffers
-  "fm" 'indent-region
-  "pF" 'helm-projectile-find-file-in-known-projects
+  "aA" 'helm-apropos "cw" 'ispell-word "wD"
+  'delete-other-windows "db" 'delete-all-other-buffers
+  "fm" 'indent-region "pF" 'helm-projectile-find-file-in-known-projects
   "ps" 'spacemacs/helm-project-smart-do-search
   "nt" 'neotree-toggle
-  ;;; o is for org mode
-  "ds" 'delete-trailing-whitespace
-  "Z"  'current-term)
+;;; o is for org mode "ds" 'delete-trailing-whitespace
+  "Z" 'current-term)
 
 (define-key evil-insert-state-map (kbd "C-y") 'helm-yas-complete)
 (define-key evil-normal-state-map (kbd "C-f") 'evil-jump-forward)
 (define-key evil-insert-state-map (kbd "C-f") 'evil-jump-forward)
+(define-key evil-insert-state-map (kbd "M-i") 'pyim-convert-pinyin-at-point)
 
 (defun find-term-buffer ()
   (interactive)
@@ -33,20 +27,18 @@
 
 ;;; this is the setting for kill buffer without confirmation
 ;;; from http://emacs.stackexchange.com/questions/14509/kill-process-buffer-without-confirmation
-(setq kill-buffer-query-functions (delq 'process-kill-buffer-query-function kill-buffer-query-functions))
+(setq kill-buffer-query-functions (delq 'process-kill-buffer-query-function
+                                        kill-buffer-query-functions))
 
-(defun kill-term-window-and-process()
-   (switch-to-buffer (buffer-name (find-term-buffer)))
-   (term-kill-subjob)
-   (kill-buffer (find-term-buffer))
-  )
+(defun kill-term-window-and-process ()
+  (switch-to-buffer (buffer-name (find-term-buffer)))
+  (term-kill-subjob)
+  (kill-buffer (find-term-buffer)))
 
 (defun kill-term ()
   (interactive)
   (if (find-term-buffer)
-      (kill-term-window-and-process)
-      )
-  )
+      (kill-term-window-and-process)))
 
 
 
@@ -54,8 +46,8 @@
   (interactive)
   (if (find-term-buffer)
       (switch-to-buffer (buffer-name (find-term-buffer)))
-    (ansi-term (getenv "SHELL") "editing-file-root")
-    ))
+    (ansi-term (getenv "SHELL")
+               "editing-file-root")))
 
 (provide 'init-global-keybinds)
 ;;; init-global-keybinds.el end here
