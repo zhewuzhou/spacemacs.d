28900308df109903de20064955e4da31d9306f11
zhewuzhou <zhewuzhou@gmail.com>
2017-01-24 20:43:50 +1100

    [Z.Z][Spacemacs] remove useless configuration for projectile

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -4,12 +4,5 @@
 
 (setq neo-show-hidden-files nil)
 
-(setq projectile-switch-project-action '(lambda()
-                                          (interactive)
-                                          (neotree-projectile-action)
-                                          (neotree-hide)
-                                          (kill-term)
-                                          (projectile-find-file)))
-
 (provide 'init-project)
 ;;; init-project.el end here
