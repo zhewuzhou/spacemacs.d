b8735ecfedb68aca7ec7fceb46a1783d6466735e
zhewuzhou <zhewuzhou@gmail.com>
2017-01-26 22:30:24 +1100

    [Z.Z][Spacemacs] make term works as project changed

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -4,5 +4,13 @@
 
 (setq neo-show-hidden-files nil)
 
+(setq projectile-switch-project-action '(lambda()
+                                          (interactive)
+                                          (neotree-projectile-action)
+                                          (neotree-hide)
+                                          (kill-term)
+                                          (projectile-find-file)))
+
+
 (provide 'init-project)
 ;;; init-project.el end here
