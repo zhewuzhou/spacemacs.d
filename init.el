f5b5c8f39d3ac7af01c6b965f830cbccaf0c6640
zhewuzhou <zhewuzhou@gmail.com>
2017-12-12 11:09:10 +0800

    [Z.Z][Spacemacs] Add support for export org as gfm

--- init.el
+++ init.el
@@ -70,8 +70,8 @@ values."
               t)
          yaml
          markdown
-         org
-         )
+         (org :variables org-enable-github-support t)
+  )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
